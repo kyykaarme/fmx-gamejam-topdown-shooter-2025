009431a92728cc19f3dd7012ce3fc827a646804a
annguyen0601 <quocan0601@gmail.com>
2025-05-03 17:16:35 +0200

    Shooting and Enemies Spawning Mechanism Added

--- Scenes/Enemies/enemies.gd
+++ Scenes/Enemies/enemies.gd
@@ -1,48 +1,20 @@
 extends CharacterBody3D
 
-@onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
-@onready var raycast: RayCast3D = $RayCast3D
-@onready var timer: Timer = $Timer
-var speed: float = 5.0
-var direction: Vector3 = Vector3.ZERO
-var player: CharacterBody3D
-var health: int = 50
-
-
-func _ready():
-	# Get player node (adjust path if your scene structure differs)
-	var players = get_tree().get_nodes_in_group("player")
-	if players.size() > 0:
-		player = players[0]
-	# Set initial target location to player
-	if player:
-		navigation_agent.set_target_position(player.global_position)
-	# Connect timer for periodic path updates
-	timer.timeout.connect(_on_timer_timeout)
-
-func _physics_process(delta):
-	if player:
-		# Get current and next positions for navigation
-		var current_position = global_position
-		var next_position = navigation_agent.get_next_path_position()
-		direction = (next_position - current_position).normalized()
-		velocity = direction * speed
-		# Smooth movement to avoid jitter
-		velocity = velocity.move_toward(velocity, 0.25)
-		move_and_slide()
-		
-		# Attack logic with raycast
-		if raycast.is_colliding() and raycast.get_collider() == player:
-			player.take_damage(10)  # Ensure Player has take_damage method
-
-func _on_timer_timeout():
-	if player and global_position.distance_to(player.global_position) < 100:
-		# Update navigation target to player's current position
-		navigation_agent.set_target_position(player.global_position)
-
-func take_damage(amount: int):
-	health -= amount
-	if health <= 0:
-		var spawner = get_tree().current_scene.get_node("EnemySpawner")
-		spawner.enemy_count -= 1
-		queue_free()
+@onready var nav = $NavigationAgent3D
+var speed = 3.5
+var gravity = 9.8
+func _process(delta):
+	if not is_on_floor():
+		velocity.y -= gravity*delta
+	else:
+		velocity.y -= 2
+	var next_location = nav.get_next_path_position()
+	var current_location = global_transform.origin
+	var new_velocity = (next_location - current_location).normalized() * speed
+	
+	velocity = velocity.move_toward(new_velocity, 0.25)
+	move_and_slide()
+	#get_tree().call_group("level", "enemy_death")
+	
+func target_position(target):
+	nav.target_position = target
---
+++ levels/main.gd
@@ -0,0 +1,74 @@
+extends Node3D
+
+
+@onready var target = $Player
+
+func _process(delta):
+	get_tree().call_group("enemies", "target_position", target.global_transform.origin)
+
+#essetially just a counter
+@onready var current_level=0
+#the level is the key the amount of monsters is the value
+@onready var monster_dict={
+ 1:1,
+ 2:2,
+ 3:4,
+ 4:8,
+ 5:30
+}
+
+#the monster we will be spawning in. 
+@onready var monster=preload("res://Scenes/Enemies/enemies.tscn")
+#A random number genrerator to spawn from alternating spawn points.
+@onready var rand=RandomNumberGenerator.new()
+@onready var dead_enemies=0
+
+func _ready():
+	add_to_group("level")
+
+func enemy_death():
+	print("enemy death")
+	dead_enemies+=1
+	if dead_enemies==monster_dict[current_level]:
+		$InBetweenWaves.start()
+		dead_enemies=0
+ #get_tree().call_group("player", "award_points", 200)
+
+func spawn_enemies():
+	for i in range(monster_dict[current_level]):
+		var m = monster.instantiate()
+		print("spawning enemy")
+	  #we check the amount of children on our spawn holder 
+		var spawn_length = $SpawnHolder.get_child_count()-1
+	  #then make a random number in that range
+		var rand_num = rand.randi_range(0,spawn_length)
+	  #We use that number to randomly select a spawner node position to use 
+		var spawn_postion = $SpawnHolder.get_child(rand_num).position
+	  #we add the monster as a child of the level
+	  #We set the monsters position to the spawn location
+		m.position = spawn_postion
+		add_child(m)
+
+func update_level(level):
+	match level:
+		1:
+			print("its level one")
+	   #additional stuff here like announcer saying: Wave 1!
+		2:
+			print("its level two")
+		3:
+			print("its level three")
+		4:
+			print("its level four")
+		5:
+			print("its level five")
+	spawn_enemies()
+
+
+
+func _on_in_between_waves_timeout():
+	print("Leaving level: ", current_level)
+	current_level+=1
+	update_level(current_level)
+
+#This is the code on the enemy script that calls the enemy death function.
