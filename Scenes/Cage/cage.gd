3ca59a5c47d499abf82ac068b11b5a5792101edf
annguyen0601 <quocan0601@gmail.com>
2025-05-04 23:17:24 +0200

    conflict

--- Scenes/Cage/cage.gd
+++ Scenes/Cage/cage.gd
@@ -1,9 +1,11 @@
 extends Node3D
 
-
 @onready var FX = $Hearts
+@onready var anim_player = $AnimationPlayer
+@onready var cage = $CageCylinder007
 
-#Take Hit
+var death_anim : Animation = preload("res://Models/anim/Open.res")
+
 func _on_stats_you_died_signal():
 	FX.emitting = true  # start particle effect
 	$AnimationPlayer.play("Open")
