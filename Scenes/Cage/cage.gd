34884b78affce25e523e087cb300b204b9863ef7
annguyen0601 <quocan0601@gmail.com>
2025-05-04 23:02:15 +0200

    Cage open

--- Scenes/Cage/cage.gd
+++ Scenes/Cage/cage.gd
@@ -6,6 +6,4 @@ extends Node3D
 #Take Hit
 func _on_stats_you_died_signal():
 	FX.emitting = true  # start particle effect
-	$CageCylinder007.visible = false
-	await get_tree().create_timer(1.5).timeout  # wait 0.5 seconds
-	queue_free()
+	$AnimationPlayer.play("Open")
