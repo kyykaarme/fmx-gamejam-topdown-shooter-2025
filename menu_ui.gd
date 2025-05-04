9fb467f674875e9cdd02beabccfa350fac4292f5
annguyen0601 <quocan0601@gmail.com>
2025-05-04 17:02:36 +0200

    Restructure for Menu

--- menu_ui.gd
+++
@@ -1,9 +0,0 @@
-extends CanvasLayer
-func _on_PlayButton_pressed():
-	get_tree().change_scene("res://levels/node_3d.tscn")
-
-func _on_SettingsButton_pressed():
-	get_tree().change_scene("res://settings.tscn")
-
-func _on_CreditsButton_pressed():
-	get_tree().change_scene("res://credits.tscn")
