2388a87fa2e46110100e36b44e2262c9a60f282f
sebiguitar <sebastian.schmeiser@gmx.de>
2025-05-04 21:58:20 +0200

    enemies code fix

--- Scenes/Enemies/enemies.gd
+++ Scenes/Enemies/enemies.gd
@@ -50,7 +50,7 @@ func move_and_attack():
 #Take Hit
 func _on_stats_you_died_signal():
 	FX.emitting = true  # start particle effect
-	$AnimatedSprite3D.visible = false
+	$AnimatedSprite3D.visible = false 
 	await get_tree().create_timer(1.5).timeout  # wait 0.5 seconds
 	queue_free()
 
